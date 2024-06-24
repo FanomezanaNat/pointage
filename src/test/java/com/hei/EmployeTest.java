12d940d801fb8b25dc1153c0fb7589d26faccfa6
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-24 13:23:56 +0300

    wip:test: test grossSalary and netSalary with majoration

--- src/test/java/com/hei/EmployeTest.java
+++ src/test/java/com/hei/EmployeTest.java
@@ -3,7 +3,6 @@ package com.hei;
 import com.hei.calendrier.Calendar;
 import com.hei.category.Category;
 import com.hei.category.Guard;
-import com.hei.category.Type;
 import com.hei.category.WorkingHour;
 import org.junit.Test;
 
@@ -21,36 +20,25 @@ public class EmployeTest {
     public void test() {
         int year = 2024;
         int month = 6;
-        List<LocalDate> dayOff = List.of(
-                LocalDate.of(2024, 6, 17),
-                LocalDate.of(2024, 6, 25),
-                LocalDate.of(2024, 6, 26)
-        );
+        List<LocalDate> dayOff = List.of(LocalDate.of(2024, 6, 17), LocalDate.of(2024, 6, 25), LocalDate.of(2024, 6, 26));
 
-        List<DayOfWeek> workingDays = List.of(
-                DayOfWeek.MONDAY,
-                DayOfWeek.TUESDAY,
-                DayOfWeek.WEDNESDAY,
-                DayOfWeek.THURSDAY,
-                DayOfWeek.FRIDAY,
-                DayOfWeek.SATURDAY,
-                DayOfWeek.SUNDAY
+        List<DayOfWeek> workingDays = List.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY
 
         );
 
         Calendar calendrier = new Calendar(month, year, dayOff);
-        Double salaire = 100000.00;
-        Category normalCategory = new Guard("x", 8, 100.00, 0.0);
-        Category nightCategory = new Guard("x", 8, 100.00, 0.0);
+        Category category = new Guard("x", 56, 110_000,0, workingDays,calendrier);
         Instant dateNaissance = Instant.parse("1980-01-01T00:00:00Z");
         Instant dateDembauche = Instant.parse("2015-05-20T00:00:00Z");
         Instant finContrat = Instant.parse("2023-12-31T00:00:00Z");
-        Employe Rakoto = new Employe("Rakoto", "x", dateNaissance, dateDembauche, finContrat, salaire, normalCategory, List.of(), calendrier, workingDays);
-        Employe Rabe = new Employe("Rabe", "x", dateNaissance, dateDembauche, finContrat, salaire, nightCategory, List.of(), calendrier, workingDays);
+        Employe Rakoto = new Employe("Rakoto", "x", dateNaissance, dateDembauche, finContrat, category);
+        Employe Rabe = new Employe("Rabe", "x", dateNaissance, dateDembauche, finContrat, category);
 
 
-        assertEquals(216, Rakoto.calculateMonthlyHours());
-        assertEquals(216, Rabe.calculateMonthlyHours());
+
+        assertEquals(216,Rakoto.getCategory().calculateMonthlyHours());
+        assertEquals(216,Rabe.getCategory().calculateMonthlyHours());
+
 
         var juin25 = LocalDate.of(2024, 6, 25);
         var juin26 = LocalDate.of(2024, 6, 26);
@@ -58,14 +46,21 @@ public class EmployeTest {
         var nuit = new WorkingHour(8, Arrays.asList(Nuit, JourFerie));
 
 
-        Rakoto.addWorkingOff(juin25, jour);
-        Rakoto.addWorkingOff(juin25, nuit);
-        Rakoto.addWorkingOff(juin26, jour);
-        Rakoto.addWorkingOff(juin26, nuit);
+        Rakoto.addWorkingOff(juin25, Arrays.asList(jour, nuit));
+        Rakoto.addWorkingOff(juin26, Arrays.asList(jour, nuit));
+
+        var salaryBasic = 2_970_000;
+        var netSalaryBasic=2_376_000;
+        var grossSalaryWithMajo=3_916_000;
+        var netSalaryWithMajo=3_132_800;
 
 
-        System.out.println(Rakoto.getNetSalary());
-        System.out.println(Rakoto.getWorkingDayOff());
+        assertEquals(salaryBasic,Rakoto.getCategory().getGrossSalary());
+        assertEquals(netSalaryBasic,Rakoto.getCategory().getNetSalary());
+        assertEquals(grossSalaryWithMajo,Rakoto.grossSalaryDue());
+        assertEquals(netSalaryWithMajo,Rakoto.netSalaryDue());
+
+
 
 
     }
