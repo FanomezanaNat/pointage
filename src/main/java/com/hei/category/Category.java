dbd953c4997d42cceae9003881b4d1e1f20ecc3a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-27 13:31:01 +0300

    chore: update category

--- src/main/java/com/hei/category/Category.java
+++ src/main/java/com/hei/category/Category.java
@@ -17,11 +17,11 @@ public abstract sealed class Category permits Driver, Guard, Normal, SeniorExecu
     private int salaryPerWeek;
     private int compensation;
     private List<DayOfWeek> workingDays;
-    private int hourlyRate;
+    private double hourlyRate;
     private int workingHourPerDay;
     private Calendar calendar;
-    private int grossSalary;
-    private int netSalary;
+    private double grossSalary;
+    private double netSalary;
 
 
     public Category(String name, int workingHourPerWeek, int salaryPerWeek, int compensation, List<DayOfWeek> workingDays, Calendar calendar) {
@@ -30,28 +30,22 @@ public abstract sealed class Category permits Driver, Guard, Normal, SeniorExecu
         this.salaryPerWeek = salaryPerWeek;
         this.compensation = compensation;
         this.workingDays = workingDays;
-        this.hourlyRate = (salaryPerWeek * this.workingDays.size() / workingHourPerWeek);
+        this.hourlyRate = ((double) salaryPerWeek / workingDays.size());
         this.workingHourPerDay = workingHourPerWeek / this.workingDays.size();
         this.calendar = calendar;
-        this.grossSalary = normalSalaryPerMonth();
-        this.netSalary = (int) (this.grossSalary - (this.grossSalary * 0.2));
     }
 
-    public List<LocalDate> nonStopWorkingDay() {
-        LocalDate startDate = LocalDate.of(calendar.getYear(), calendar.getMonth(), 1);
-        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+    public List<LocalDate> nonStopWorkingDay(LocalDate begin, LocalDate ending) {
+        LocalDate startDate = LocalDate.of(calendar.getYear(), begin.getMonth(), begin.getDayOfMonth());
+        LocalDate endDate = LocalDate.of(calendar.getYear(), ending.getMonth(), ending.getDayOfMonth());
 
         return startDate.datesUntil(endDate.plusDays(1))
                 .filter(date -> this.getWorkingDays().contains(date.getDayOfWeek()) && !this.calendar.getPublicHoliday().contains(date))
                 .toList();
     }
-    public int calculateMonthlyHours() {
-        return (this.nonStopWorkingDay().size()) * this.getWorkingHourPerDay();
-    }
-    private int normalSalaryPerMonth(){
-        return (int) (this.getHourlyRate() * calculateMonthlyHours());
 
-    }
+
+
 
 
 }
