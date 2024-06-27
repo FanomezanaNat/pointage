3455707e5acba2bd5edf0d14e75c748c8a3293d3
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-06-27 13:30:19 +0300

    feat: addPublicHoliday ,  addWorkingDays for calendar

--- src/main/java/com/hei/calendrier/Calendar.java
+++ src/main/java/com/hei/calendrier/Calendar.java
@@ -1,16 +1,31 @@
 package com.hei.calendrier;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-@AllArgsConstructor
+
 @Getter
 public class Calendar {
-    private  int month;
     private  int year;
+    private List<LocalDate> workingDays;
     private  List<LocalDate> publicHoliday;
+
+    public Calendar(int year) {
+        this.year = year;
+        this.workingDays=new ArrayList<>();
+        this.publicHoliday = new ArrayList<>();
+    }
+
+    public List<LocalDate> addPublicHoliday(List<LocalDate> date){
+        this.getPublicHoliday().addAll(date);
+        return this.getPublicHoliday();
+    }
+    public List<LocalDate> addWorkingDays(List<LocalDate> dates){
+        this.getWorkingDays().addAll(dates);
+        return this.getWorkingDays();
+    }
 
 }
 
